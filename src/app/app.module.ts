59633080685de5bccbbec7ba4b27f2d277267e07
AdielsonMMartins <adielson@outlook.com>
2023-04-17 20:06:57 -0300

    criaçao de novo componente para fazer a separaçao dos detalhes de cada heroi do template heroes.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 
 
@@ -12,6 +13,7 @@ import { HeroesComponent } from './heroes/heroes.component';
   declarations: [
     AppComponent,
     HeroesComponent,
+    HeroDetailComponent,
   ],
   imports: [
     BrowserModule,
